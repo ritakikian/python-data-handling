29eb5fe3460ef551ab00d43496dcef96af886956
Rita Kikian <kikianrita@gmail.com>
2025-02-28 14:01:50 +0200

    Added a function that counts the total number of employees in the company and printed it

--- employee-management.py
+++ employee-management.py
@@ -13,3 +13,7 @@ print("Company Employees: ", company_employees)
 #add new employee
 company_employees["Engineering"]["David"] = {"age": 27, "role": "Data Scientist"}
 print("Company Employees after adding David: ", company_employees)
+
+#print total number of employees
+total_employees = sum(len(dept) for dept in company_employees.values())
+print("Total Employees:", total_employees)
