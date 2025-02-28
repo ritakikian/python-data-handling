d6b5839aa41d4260a6ccce204d37b9ecd355a72d
Rita Kikian <kikianrita@gmail.com>
2025-02-28 13:49:01 +0200

    Created a dictionary that stores info about employees and printed them

---
+++ employee-management.py
@@ -0,0 +1,11 @@
+company_employees = {
+    "Engineering": {
+        "Alice": {"age": 30, "role": "Software Engineer"},
+        "Bob": {"age": 28, "role": "DevOps Engineer"}
+    },
+    "HR": {
+        "Charlie": {"age": 35, "role": "HR Manager"}
+    }
+}
+#print company employees
+print("Company Employees: ", company_employees)
